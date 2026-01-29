52316f0baa3795784994a35cf6784d3ccc9e4e96
Anupam Singh <anupamsingh505011@gmail.com>
2026-01-29 21:43:01 +0530

    Initial commit

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    //
+
+    public function list(){
+        return ['name'=>'Anupam Developer', 'email'=>'anb@gmail.com'];
+}
+
+
+ }
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/post', function(){
+
+  return ['name'=>'Anupam Developer', 'email'=>'abc@gmail.com', 'phone'=>'1234567890'];
+
+
+// $data = [
+//     'name'  => 'Anupam Developer',
+//     'email' => 'abc@gmail.com',
+//     'phone' => '1234567890'
+// ];
+
+// return response()->json($data);
+
+
+});
+
+Route::get('/student', [App\Http\Controllers\StudentController::class, 'list']);
